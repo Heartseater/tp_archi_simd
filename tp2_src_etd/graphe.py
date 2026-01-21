3c6cc4d0fbbd9501d4401aac99fbacebe5a07498
Yahya <102614726+Heartseater@users.noreply.github.com>
2026-01-21 23:22:58 +0100

    Delete tp2_src_etd/graphe.py

--- tp2_src_etd/graphe.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-labels = ["float", "int", "mmx", "sse", "unrolled"]
-temps_real = [0.006, 0.006, 0.007, 0.006, 0.006]
-temps_user = [0.000, 0.003, 0.001, 0.001, 0.001]
-temps_sys  = [0.005, 0.003, 0.004, 0.004, 0.004]
-
